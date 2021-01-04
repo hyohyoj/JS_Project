659aa9fe6f26b99267d5130418fe4d042f022da8
hyo <vlsjt3@naver.com>
2021-01-04 22:46:13 +0900

    2021-01-04 두번째 커밋

--- greeting.js
+++ greeting.js
@@ -5,8 +5,24 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);    //브라우저 localStorage (Key, Value)
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //자동으로 일어나는 이벤트를 막음.
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN); //클래스 추가
+    form.addEventListener("submit", handleSubmit);  //submit할 때 handleSubmit 함수 작동.
+}
+
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
+    form.classList.remove(SHOWING_CN);  //클래스 제거
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
@@ -14,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
-        // she is not
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
